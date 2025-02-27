07d6bd3dfacd5aa76721913a74cee849e53b958f
BrundhaBH <gowdabrundha2000@gmail.com>
2025-02-27 12:01:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import path, { join } from "path";
+import connectToMongo from "./Database/db.js";
+import authRoute from "./Routes/authRoute.js"
+import cors from "cors";
+dotenv.config();
+
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT_SECRET is not defined in the environment variables");
+}
+const app = express();
+connectToMongo();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+const __dirname = path.resolve();
+app.set("views", path.join(__dirname, "Views"));
+app.use("/auth",authRoute)
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/contact",(req ,res)=>{
+  try{
+    res.render("contact");
+    }
+    catch(err){
+    console.log(err)
+    }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port 127.0.0.1:${process.env.PORT}`);
+});
