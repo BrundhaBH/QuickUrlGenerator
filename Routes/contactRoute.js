ea0897664bd02735bdf8540fcccc1afe20bf645d
BrundhaBH <gowdabrundha2000@gmail.com>
2025-03-08 13:14:10 +0530

    update

--- Routes/contactRoute.js
+++ Routes/contactRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getContacts,createContact} from "../controllers/contactController.js";
+import { getContacts,createContact} from "../Controllers/contactController.js";
 const router = express.Router();
 
 router.get("/page/contact", getContacts);
